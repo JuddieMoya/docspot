0db8ff4765c686c25bf8632c42959b5ce52ae2a6
Vincent St. Louis <vincentst.louis@gmail.com>
2020-06-01 23:57:21 -0400

    add comments to the connected-router

--- src/components/connected-route/ConnectedRoute.js
+++ src/components/connected-route/ConnectedRoute.js
@@ -3,6 +3,11 @@ import ProptTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
+/*
+ * ConnectedRoute is a component that renders Routes for you
+ * It uses the auth store in redux to determine if a route should be rendered
+ * or redirected else where based on the auth status
+ */
 export function ConnectedRoute({
   isProtected,
   redirectIfAuthenticated,
