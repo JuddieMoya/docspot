422bb39eb865dba5a3f2b8a4c11eecbf571f03b6
Jacob Knaust <jjknaust1s@gmail.com>
2020-09-09 09:43:49 -0600

    Get-User-Singular functionality completed.

--- src/components/get-username/GetUsername.js
+++ src/components/get-username/GetUsername.js
@@ -1,46 +1,21 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/get-username";
-// ****Maybe this**** // import { actions } from "../../redux/actions/auth";
-import { Loader } from "../loader";
-import defaultImage from '../get-users/img/broken.png'
+// import defaultImage from '../get-users/img/broken.png'
 // import "./GetUsername.css";
 const PHOTO_URL = username => `https://kwitter-api.herokuapp.com${username}`
 export const GetUsername = () => {
-    // is this right state.users.username
-// *****this Maybe **** // 
-const isAuthenticated = useSelector((state) => !!state.auth.isAuthenticated)
-    const state = useSelector((state) => state.users)
+    const state = useSelector((state) => state)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(actions.fetchUsernameAction());
     }, []);
     // console.log(state)
-    
-    // 1.)If event.target.value from login from = username display name and photo
-    // 2.) is Authenticated?
-
-    
     return(
-        <React.Fragment>
-            {isAuthenticated ?(
-                <>
-            {state.users.map(user => {
-                    let userImage= defaultImage
-                    if(user.pictureLocation!==null){
-                        userImage= PHOTO_URL(user.pictureLocation)
-                    }
-                    return (
-                    <p>
-                        {user.username}
-                        {/* {user} */}
-                        <img
-                            alt="Profile"
-                            src={userImage}
-                        />
-                    </p>)})}
-                    </>
-          ) : null}
-        </React.Fragment>
+        
+        <>
+        <h1>{state.auth.username}</h1>
+        
+        </>
     )
 }
