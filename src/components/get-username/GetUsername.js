6d98ee8e73bef710ea079f8f05be36e8afd98eb5
Jacob Knaust <jjknaust1s@gmail.com>
2020-09-14 13:37:56 -0600

    profile page style completed

--- src/components/get-username/GetUsername.js
+++ src/components/get-username/GetUsername.js
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/get-username";
 import defaultImage from '../get-users/img/broken.png'
 import ProfilePicLoader from '../profile-pic-loader/ProfilePicLoader'
-// import "./GetUsername.css";
+import {Card} from 'react-bootstrap'
+import "./GetUsername.css";
 const PHOTO_URL = username => `https://kwitter-api.herokuapp.com${username}`
 export const GetUsername = () => {
     const state = useSelector((state) => state)
@@ -17,13 +18,15 @@ export const GetUsername = () => {
     return(
         <>
         {state.getUsername.currentUser&&(
-        <div>
-        <h1>{state.auth.username}</h1>
+        <Card 
+        style={{backgroundColor:'rgb(29, 131, 181)'}}
+        className='user-info'>
+        <Card.Title>{state.auth.username}</Card.Title>
         <img
         alt='profile pic'
         src = {PHOTO_URL(state.getUsername.currentUser.user.pictureLocation)}
         />
-        </div>
+       </Card>
         )}
         </>
     )
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { MenuContainer } from "../components";
 import { GetUsernameContainer } from "../components/";
-// import { ProfilePicLoaderContainer } from "../components/";
 import {Link} from 'react-router-dom'
 import {Card, Nav} from 'react-bootstrap'
 
@@ -10,7 +9,6 @@ export const ProfileScreen = () => (
     <MenuContainer />
     <h1 style = {{color:'white', textAlign:'center'}}>Profile Homepage</h1>
     <GetUsernameContainer />
-    {/* <ProfilePicLoaderContainer /> */}
     <h2 style={{ textAlign:'center', color:'white'}}>About</h2>
     <p style={{height:'100vh', color:'white'}}> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
   <Card.Footer style={{display:'flex', justifyContent:'center'}}>
