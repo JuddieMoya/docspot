bf031ebb9d0947bb16d9bbd4d9181268e4d0b627
Eleanor Hall <ruthhall97@gmail.com>
2020-10-09 13:49:39 -0700

    firebase.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { Navigation } from "./components";
-import configureStore from "./redux/configureStore";
 
-const { store, persistor } = configureStore();
 
-const DocSpot = {
-  store,
-  persistor,
-};
 
-window.DocSpot = DocSpot;
 
-export const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Navigation />
-    </PersistGate>
-  </Provider>
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { Navigation } from "./components";
+// import configureStore from "./redux/configureStore";
+
+// const { store, persistor } = configureStore();
+
+// const DocSpot = {
+//   store,
+//   persistor,
+// };
+
+// window.DocSpot = DocSpot;
+
+// export const App = () => (
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <Navigation />
+//     </PersistGate>
+//   </Provider>
+// );
