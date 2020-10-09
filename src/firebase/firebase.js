b2c392e70aa000d1e7188d0ceb120fe78e093c46
Jeyquan Johnson <fatjey@Jeyquans-Air.fios-router.home>
2020-10-09 11:43:55 -0400

    first add of firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth'
+
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyCZkdh88GpxA2Snpw4jITrf28GEigELVeA",
+    authDomain: "kapstone-n.firebaseapp.com",
+    databaseURL: "https://kapstone-n.firebaseio.com",
+    projectId: "kapstone-n",
+    storageBucket: "kapstone-n.appspot.com",
+    messagingSenderId: "500997011347",
+    appId: "1:500997011347:web:8d0d36af03259e6bf09a66",
+    measurementId: "G-EELYLHGD70"
+  });
+
+  firebase.analytics();
+
+  export default app
