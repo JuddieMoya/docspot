a11aaf6d811335b064fc65064b78e33a6c5c9661
Eleanor Hall <ruthhall97@gmail.com>
2020-10-12 12:16:01 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Navigation } from "./components";
+import configureStore from "./redux/configureStore";
 
+const { store, persistor } = configureStore();
 
+const DocSpot = {
+  store,
+  persistor,
+};
 
+window.DocSpot = DocSpot;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { Navigation } from "./components";
-// import configureStore from "./redux/configureStore";
-
-// const { store, persistor } = configureStore();
-
-// const DocSpot = {
-//   store,
-//   persistor,
-// };
-
-// window.DocSpot = DocSpot;
-
-// export const App = () => (
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>
-//       <Navigation />
-//     </PersistGate>
-//   </Provider>
-// );
+export const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+    </PersistGate>
+  </Provider>
+);
